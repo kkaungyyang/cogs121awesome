8bc67fd2ca2a0d58cfbbfa65b668b6bf05adaab5
Andy Or <awor@ucsd.edu>
2019-04-30 20:02:18 -0700

    Firebase set up. Credentials in local json file.

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const express = require('express'); 
 const bodyParser = require('body-parser'); 
 const path = require('path');
+const firebase = require("firebase");
+const admin = require('firebase-admin');
+const firebaseConfig = require('./firebase-credentials.json')
 const app = express(); 
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const firebaseDB = firebase.database(); // reference to Firestore Realtime DB
+// for (breed of Object.keys(fakeDatabase) ) {
+//   db.collection('facts').doc(breed).set(fakeDatabase[breed]); 
+// }
+const writeToDB =  false;
+if (writeToDB) { // Upload data from dog breeds API
+ 
+  let breed = 'Irish Terrier2';
+  firebaseDB.ref('breedInfo/' + breed).set({ // Add info on each breed of dog to DB
+    height: {
+      "imperial": "18",
+      "metric": "46"
+    },
+    life_span: "12 - 16 years",
+    temperament: "Respectful, Lively, Intelligent, Dominant, Protective, Trainable",
+    weight: {
+      "imperial": "25 - 27",
+      "metric": "11 - 12"
+    }
+  }).then(
+    console.log("Successful write to firebase database!")
+  );
+}
 
 app.set('views', path.join(__dirname, 'views')); // specify that we want to use "views" folder for our "HTML" templates
 app.set('view engine', 'ejs'); // specify our "view engine" will be ejs, aka let's use ejs
@@ -46,7 +76,13 @@ app.get('/search/:animal', function(req,res){
 });
 
 //temp hardcode ajax fetch for breed match
-app.get('/fetch', function(req,res){
+app.get('/fetch', function(req,res) {
+  // db.collection('facts').doc(breed).get().then( doc => { // TODO: Consider mix of breeds
+  //   res.send(doc)
+  // }).catch( err => {
+  //   console.log('Error getting info on breed', err)
+  // });
+
 	res.send({"Golden Retriever": "Loyal happy breed.", "Lab": "Kind happy breed."});
 });
 
